20ce70ccc73daf6bc0a17f5976ce62786f5fbf6d
Dmitry Kutin <dmitrykutin@outlook.com>
2021-01-16 14:23:53 -0500

    Update project structure

---
+++ main.py
@@ -0,0 +1,8 @@
+# Import helper functions
+import preprocess from './src/preprocess'
+
+def main():
+    print("CSI 4107 - Microblog information retrieval system")
+
+if __name__ == "__main__":
+    main()
--- preprocess.py
+++ preprocess.py
@@ -1,22 +1,30 @@
+# Main imports.
 import string
+import nltk
 
+# Import specific packages.
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-# Not sure how to add these yet, 
-# had to do it through `python3`
-################################
-# nltk.download('stopwords')
-# nltk.download('punkt)
+# Download packages if not installed locally.
+nltk.download('stopwords')
+nltk.download('punkt')
 
-# This filters tweets and queries.
 def filterSentence(sentence):
-    custom_stopwords = set(stopwords.words('english')).union((line.strip('\r\n') for line in open('./assets/stopwords.txt', 'r')))
+    ''' 
+    Filters sentences from tweets and queries.
 
-    tokens = [word.lower() for word in word_tokenize(sentence) 
+    :param str sentence: The sentence to be tokenized with stopwords and punctuation removed.
+    :return: the tokenized sentence.
+    :rtype: string
+    '''
+    custom_stopwords = set(stopwords.words('english')).union((line.strip('\r\n') for line in open('./assets/stop_words.txt', 'r')))
+
+    tokens = [word.lower() for word in word_tokenize(sentence)
         if not word in custom_stopwords and not word in string.punctuation]
 
     return tokens
 
+# Example test case, will still have to load the data.
 for word in filterSentence("""I'd very much appreciate it if people would stop broadcasting asking me to add people on BBM."""):
     print (word)
