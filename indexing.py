c533c06fce0a9f01e3c631d036fef24a369dc026
Joshua Erivwo <erivwoganre@gmail.com>
2021-02-06 22:01:56 -0500

    Delete indexing.py

--- indexing.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict
-
-def create_index (data):
-        index = defaultdict(list)
-        for i, tokens in enumerate(data):
-            for token in tokens:
-                index[token].append(i)
-        return index
